b4d1d94db276db2cca22f2398da7fafbd4784f7c
Arca Ege Cengiz <40526225+ArcaEge@users.noreply.github.com>
2024-06-08 00:39:50 +0000

    aa

--- initialise.sh
+++ initialise.sh
@@ -3,7 +3,6 @@ sudo docker run --name=fedora-vnc -d --rm \
         --tmpfs /run --tmpfs /run/lock --tmpfs /tmp \
         --cgroupns=host --cap-add SYS_BOOT --cap-add SYS_ADMIN \
         -v /sys/fs/cgroup:/sys/fs/cgroup \
-        -v vol:/ \
         -p 5901:5901 -p 6901:6901 \
         arcaege/fedora-gnome-vnc
 
